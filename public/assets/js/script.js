815ddde8fdb9c8611c8d0d2a8c845629c8b5ed8e
B-Helgeson <brandonstevenhelgeson@gmail.com>
2018-06-17 16:50:39 -0400

    updated base config

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
     
+    // Enable the floating action button, for the main menu
+    $('.fixed-action-btn').floatingActionButton();
 
+    // Enable the sidenav, for the main menu
+    $('.sidenav').sidenav();
+
+    // Enable the collabsable, for the UI Form
+    $('.collapsible').collapsible();
+
+    // Enable the Date and Time Picker functionality
+    $('.datepicker').datepicker();
+    $('.timepicker').timepicker();
 
 
 
