28ca976fdcf9f7530567a96c47cea44f8a77d2ee
B-Helgeson <brandonstevenhelgeson@gmail.com>
2018-07-26 15:26:35 -0400

    updated routing in nav bar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -12,12 +12,14 @@ const Nav = () => (
               <a href="#name"><span className="white-text name navOverlay">John Doe</span></a>
               <a href="#email"><span className="white-text email navOverlay">jdandturk@gmail.com</span></a>
             </div></li>
-          <li><a href="#!">View Log Book</a></li>
+          <li><a className="waves-effect" href="/log">Add a Dive Log</a></li>
+          <li><a href="/profile">View My Log Book</a></li>
           <li><div className="divider" /></li>
           <li><a className="subheader">Menu</a></li>
-          <li>
-            <a className="waves-effect" href="#!">Add a Dive Log</a></li>
-          <li><a href="#!"><i className="material-icons">person</i>Edit Profile</a></li>
+          <li><a href="/login">Home</a></li>
+          <li><a href="/social">Social Page</a></li>
+          <li><a href="/acct"><i className="material-icons">person</i>Edit Profile</a></li>
+          <li><a href="/cert"><i className="material-icons">contact_mail</i>Edit Certifications</a></li>
         </ul>
         <a href="111" data-target="slide-out" className="sidenav-trigger btn-floating btn-large waves-effect waves-light blue"><i className="material-icons">menu</i></a>
   </div>
